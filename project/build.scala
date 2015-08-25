3cd45d49f2a7f97893d63db4410614c57c725bba
Bryce Anderson <bryce.anderson22@gmail.com>
2015-08-25 14:51:44 -0400

    release/0.1.3 because 0.1.2 was built with java8

--- project/build.scala
+++ project/build.scala
@@ -15,7 +15,7 @@ object ApplicationBuild extends Build {
   lazy val buildSettings = Defaults.defaultSettings ++ publishing ++ Seq(
     organization := "org.http4s",
 
-    version := "0.1.2",
+    version := "0.1.3",
 
     scalaVersion := "2.11.4",
 
